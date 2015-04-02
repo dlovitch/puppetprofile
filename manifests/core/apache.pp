86a6d0d0a41214e9acf6b9685c06d83233b0f869
David Lovitch <dlovitch@gmail.com>
2015-04-02 01:44:26 -0700

    allow http and https through the firewall

--- manifests/core/apache.pp
+++ manifests/core/apache.pp
@@ -2,6 +2,12 @@ class profile::core::apache (
   $timeout            = '120',
 ) {
 
+  firewall { '100 allow http and https access':
+    port    => [80, 443],
+    proto   => 'tcp',
+    action  => 'accept',
+  }
+
   class {'::apache':
     timeout => $timeout,
   }
